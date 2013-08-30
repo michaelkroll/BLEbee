c04137a571a19825d7156bcf4f40930d49289ab0
michaelkroll <dr.michael.kroll@gmail.com>
2013-08-30 22:14:34 +0200

    Arduino FIO v3 Sample Sketch added.

--- arduino/BLEBee_v_1_0_0_FIO3/BLEBee_v_1_0_0_FIO3.ino
+++ arduino/BLEBee_v_1_0_0_FIO3/BLEBee_v_1_0_0_FIO3.ino
@@ -1,8 +1,14 @@
 /*
  * BLEbee Test Sketch for Arduino 1.0.5
- * v1.0.0 2013-06-05
+ * v1.0.0 2013-08-30
  * 
+ * This Sketch is used to test the BLEBees communication features
+ * with a Fio v3 - ATmega32u4 from Sparkfun Electronics: https://www.sparkfun.com/products/11520
+ *
  * Copyright (c) 2013 Dr. Michael Kroll
+ *
+ * http://www.mkroll.mobi
+ * http://forum.mkroll.mobi
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
  * software and associated documentation files (the "Software"), to deal in the Software 
@@ -18,46 +24,39 @@
  * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
  * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * http://www.mkroll.mobi
- * http://forum.mkroll.mobi
  */
 
-// Pin 13 has an LED connected on most Arduino boards.
-int led = 13;
+void setup()   {
+  // Set the baudrate of the Arduino
+  Serial.begin(9600);
+  Serial.print("BLEbee v1.0.0 Sketch setup...");
 
-long previousMillis = 0; 
-long interval = 1000; 
-
-void setup()  
-{
-  // set the data rate for the Serial port
-  Serial.begin(14400);
-  randomSeed(analogRead(0));  
+  // Set the data rate for the Serial1 where the BLEbee is connected to
+  // The BLEBee's baudrate can be controlled by characteristic: 
+  // 2FBC0F31-726A-4014-B9FE-C8BE0652E982
+  // 0x00 = 9600 (default)
+  // 0x01 = 14400
+  // 0x02 = 19200
+  // 0x03 = 28800
+  // 0x04 = 38400
+  Serial1.begin(9600);
+  Serial.println(" done.");
 }
 
 void loop() // run over and over
-{ 
-  unsigned long currentMillis = millis();
- 
-  if(currentMillis - previousMillis > interval) {
-    previousMillis = currentMillis;   
-    
-    int randNumber1 = random(255);
-    int randNumber2 = random(255);
-    int randNumber3 = random(255);
-    int randNumber4 = random(255);
-    
-    Serial.write(randNumber1);
-    Serial.write(randNumber2);
-    Serial.write(randNumber3);
-    Serial.write(randNumber4);    
-  }
-  
+{
+  // Wait for Data on the serial console and submit it to the
+  // BLEBee byte per byte. there is no longer a buffer involved.
   if (Serial.available()) {
-    int data = Serial.read();
-    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);               // wait for a second
-    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+    int ch = Serial.read();
+    Serial.print(ch);
+    Serial1.write(ch);
+  }
+
+  // Wait for data send from the iPhone and print it to the serial console.
+  // That's it. Quite simple.
+  if (Serial1.available()) {
+    Serial.write(Serial1.read());
   }
 }
+
